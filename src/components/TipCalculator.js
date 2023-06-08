f9c9b58adcd9d000efe2c392dc1978815a8ebbba
Michael <michael.r.truscott@gmail.com>
2023-06-08 20:44:58 +1000

    Implement tip calculator state and logic Add disabled style for button

--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -1,46 +1,82 @@
+import { useState } from "react";
 import dollarIcon from "../images/icon-dollar.svg";
 import personIcon from "../images/icon-person.svg";
 
 export default function TipCalculator() {
+  const [numPeople, setNumPeople] = useState(null);
+  const [billAmount, setBillAmount] = useState(null);
+  const [tipPercent, setTipPercent] = useState({ percent: 0, custom: false });
+  
+  let tipAmount = "$0.00";
+  let total = "$0.00";
+  if (numPeople > 0 && billAmount >= 0) {
+    let totalTipPerPerson = billAmount * (tipPercent.percent / 100) / numPeople;
+    let totalPerPerson = billAmount / numPeople + totalTipPerPerson;
+    tipAmount = "$" + totalTipPerPerson.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2});
+    total = "$" + totalPerPerson.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2});
+  }
+  const onResetClicked = () => {
+    setNumPeople(null);
+    setBillAmount(null);
+    setTipPercent({percent: 0, custom: false});
+  };
+  const resetDisabled = numPeople === null && billAmount === null &&
+    tipPercent.percent === 0 && tipPercent.custom === false;
+  
   return (
     <div className="TipCalculator">
-      <BillAmountField />
-      <TipPercentSection />
-      <NumOfPeopleField error={true} />
-      <TotalDisplay />
+      <BillAmountField billAmount={billAmount} setBillAmount={setBillAmount} />
+      <TipPercentSection tipPercent={tipPercent} setTipPercent={setTipPercent} />
+      <NumOfPeopleField numPeople={numPeople} setNumPeople={setNumPeople} error={numPeople === 0} />
+      <TotalDisplay tipAmount={tipAmount} total={total} onResetClicked={onResetClicked} resetDisabled={resetDisabled} />
     </div>
   );
 }
 
-function BillAmountField() {
+function BillAmountField({billAmount, setBillAmount}) {
   return (
     <div className="InputGroup">
       <p>Bill</p>
       <div className="input-container">
         <img className="input-icon" src={dollarIcon} alt="" />
-        <input type="number" min={0} step={0.01} value={142.55} />
+        <input type="number" min={0} step={0.01}
+          value={billAmount ?? ""}
+          placeholder="0"
+          onChange={(e) => setBillAmount(e.target.value)}
+        />
       </div>
     </div>
   );
 }
 
-function TipPercentSection() {
+function TipPercentSection({tipPercent, setTipPercent}) {
   return (
     <div className="InputGroup">
       <p>Select Tip %</p>
       <div className="TipPercentSection__choices">
-        <button>5%</button>
-        <button>10%</button>
-        <button className="btn-highlighted">15%</button>
-        <button>25%</button>
-        <button>50%</button>
-        <input type="number" placeholder="Custom" />
+        {
+          [5,10,15,25,50].map((num, index) => {
+            return (
+              <button key={index}
+                className={(!tipPercent.custom && tipPercent.percent === num) ? "btn-highlighted" : undefined}
+                onClick={() => setTipPercent({percent: num, custom: false})}
+              >
+                {num}%
+              </button>
+            );
+          })
+        }
+        <input type="number" placeholder="Custom"
+          value={tipPercent.custom ? tipPercent.percent : ""}
+          min={0} max={100}
+          onChange={(e) => setTipPercent({percent: Number(e.target.value), custom: true})}
+        />
       </div>
     </div>
   );
 }
 
-function NumOfPeopleField({error}) {
+function NumOfPeopleField({numPeople, setNumPeople, error}) {
   return (
     <div className="InputGroup">
       <div className="flex-row">
@@ -49,13 +85,16 @@ function NumOfPeopleField({error}) {
       </div>
       <div className="input-container">
         <img className="input-icon" src={personIcon} alt="" />
-        <input className={error && "error"} type="number" value={5} />
+        <input className={error ? "error" : undefined} type="number" min={0}
+          placeholder="0"
+          value={numPeople ?? ""} onChange={(e) => setNumPeople(Number(e.target.value))}
+        />
       </div>
     </div>
   );
 }
 
-function TotalDisplay() {
+function TotalDisplay({tipAmount, total, onResetClicked, resetDisabled}) {
   return (
     <div className="TotalDisplay">
       <div className="TotalDisplay__group">
@@ -63,7 +102,7 @@ function TotalDisplay() {
           <p className="TotalDisplay__title">Tip Amount</p>
           <p className="TotalDisplay__per-person">/ person</p>
         </div>
-        <p className="TotalDisplay__amount">{"$4.27"}</p>
+        <p className="TotalDisplay__amount">{tipAmount}</p>
       </div>
 
       <div className="TotalDisplay__group">
@@ -71,10 +110,14 @@ function TotalDisplay() {
           <p className="TotalDisplay__title">Total</p>
           <p className="TotalDisplay__per-person">/ person</p>
         </div>
-        <p className="TotalDisplay__amount">{"$32.79"}</p>
+        <p className="TotalDisplay__amount">{total}</p>
       </div>
 
-      <button className={"TotalDisplay__reset-btn " + (true && "btn-highlighted")}>Reset</button>
+      <button className={"TotalDisplay__reset-btn " + (true && "btn-highlighted")} onClick={onResetClicked}
+        disabled={resetDisabled}
+        >
+          Reset
+      </button>
     </div>
   );
 }
